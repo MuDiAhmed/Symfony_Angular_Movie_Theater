1c00e0080ff4bb6e79baeaf8ff4120023dd92f9b
Mohamed Ahmed <mohamed.ahmed.abdel.monem@gmail.com>
2016-09-29 02:46:22 +0200

    clean main controller

--- src/AppBundle/Controller/MainController.php
+++ src/AppBundle/Controller/MainController.php
@@ -20,25 +20,7 @@ class MainController extends Controller
      * @Route("/")
      */
     public function viewAction(){
-
-//        $user = new Users();
-//        $user->setName('Mohamed Ahmed');
-//        $em = $this->getDoctrine()->getManager();
-//        // tells Doctrine you want to (eventually) save the Product (no queries yet)
-//        $em->persist($user);
-//
-//        // actually executes the queries (i.e. the INSERT query)
-//        $em->flush();
-//
-//        $templating = $this->container->get('templating');
-//        $html = $templating->render('test/index.html.twig',[
-//            'id' => $user->getId(),
-//            'name' => 'Mohamed Ahmed'
-//        ]);
-//        return new Response($html);
-        // replace this example code with whatever you need
         return $this->render('index.html.twig', [
-//            'base_dir' => realpath($this->getParameter('kernel.root_dir').'/..'),
         ]);
     }
 }
